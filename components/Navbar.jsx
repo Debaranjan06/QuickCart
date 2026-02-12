098dcfcf4f6c9293f39c9323c773bf59edf2dbbe
Debaranjan <debaranjan.lenka2006@gmail.com>
2026-02-12 22:48:40 +0530

    Installed lucide-react

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import { assets, BagIcon, BoxIcon, CartIcon} from "@/assets/assets";
+import { assets, BagIcon, BoxIcon, CartIcon} from "@/assets/assets.js";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
