8307c43d8c678db72f14cfb8a27c81b1197f0762
Debaranjan <debaranjan.lenka2006@gmail.com>
2026-02-16 16:41:13 +0530

    last made correct changes from .then

--- config/db.js
+++ config/db.js
@@ -1,28 +1,30 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn:null, promise: null}
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-    if (cached.conn) {
-        return cached.conn
-    }
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if (!cached.promise){
-        const opts = {
-            bufferCommands: false
-        }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-        cached.promise = (await mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts)).then(mongoose => {
-            return mongoose
-        })
-    }
+    cached.promise = mongoose.connect(
+      `${process.env.MONGODB_URI}/quickcart`,
+      opts
+    );
+  }
 
-    cached.conn = await cached.promise
-    return cached.conn
+  // Await the stored promise
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default connectDB
+export default connectDB;
